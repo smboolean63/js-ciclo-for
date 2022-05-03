0296b2cc0d11aaa25b51150bd266b328072df4b4
smboolean63 <smboolean63@madrigali.dev>
2022-05-03 14:18:33 +0200

    fizzbuzz

--- js/main.js
+++ js/main.js
@@ -1,29 +1,40 @@
+// Consegna:
+// Scrivi un programma che stampi in console i numeri da 1 a 100.
+// MILESTONE 1
+// Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+// MILESTONE 2
+// Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
+// MILESTONE 3
+// Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Come faccio a sapere se un numero è divisibile per un altro? Abbiamo visto qualcosa di particolare che possiamo usare? Come creare nuovi elementi html e appenderli al container?
+
+// Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
 const list = document.querySelector(".list");
-
-for( let i = 1; i <= 90; i++ ) {
-    const element = document.createElement("li");
-
-    element.classList.add("item");
-    element.innerText = i;
-    list.append(element);
-}
-
-// alla pressione della barra spaziatrice
-document.addEventListener("keyup", 
-    function(event) {
-        if( event.code === 'Space') {
-            // estraggo un numero = random fra 1 e 90
-            const randomNumber = Math.floor(Math.random() * 90) + 1;
-            console.log(randomNumber);
-            const item = document.querySelector(`.list .item:nth-child(${randomNumber})`);
-            if ( !item.classList.contains("item-active") ) {
-                alert(`il numero estratto è ${randomNumber}`);
-                item.classList.add("item-active");
-            } else {
-                alert("il numero è gia stato estratto");
-            }
-            // seleziono la casella corrispondente al numero estratto e gli aggiungo la classe item-active
-        }
+// 1. Scrivi un programma che stampi in console i numeri da 1 a 100.
+for( let i = 1; i <= 100; i++ ) {
+    const item = document.createElement("li"); // creo l'elemento <li></li>
+    item.classList.add("item");  // aggiungo la classe .item
+    // Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+    // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+    // assegnare un contenuto ad mio <li class="item"></li>
+    if( i % 3 === 0 && i % 5 === 0 ) {
+        console.log("fizzbuzz");
+        item.innerText = "fizzbuzz";
+        item.classList.add("fizzbuzz");
+    } else if( i % 3 === 0 ) {
+        console.log("fizz");
+        item.innerText = "fizz";
+        item.classList.add("fizz");
+    } else if ( i % 5 === 0 ) {
+        console.log("buzz");
+        item.innerText = "buzz";
+        item.classList.add("buzz");
+    } else {
+        console.log(i);
+        item.innerText = i;
     }
-);
-
+    // inserire il <li class="item">[1, fizz, buzz, fizzbuzz]</li> nella lista
+    list.append(item);
+}
